5128db3d2d5434d158b06a47ce07b9632729fe1d
elyas esna <elyasesnaashari@yahoo.com>
2024-09-03 00:18:38 +0330

    init

---
+++ FlightOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightChallenge
+{
+	internal record FlightOutput(
+		int FlightId,
+		int OriginCityId,
+		int DestinationCityId,
+		DateTime DepartureTime,
+		DateTime ArrivalTime,
+		int AirlineId,
+		FlightStatus Status
+		);
+
+	public enum FlightStatus : short
+	{
+		New,
+		Discontinued
+	}
+}
